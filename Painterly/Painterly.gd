2874fa5a6c2ed0ce0f144395d348e1cc362ed5b7
Nicola Baribeau <nicolabaribeau@gmail.com>
2025-08-25 23:40:20 -0400

    More shaders !

--- Painterly/Painterly.gd
+++ Painterly/Painterly.gd
@@ -21,6 +21,7 @@ func _get_func_code(_mode: Shader.Mode, type: VisualShader.Type) -> String:
 	match type:
 		VisualShader.Type.TYPE_VERTEX:
 			return "
+				VERTEX = VERTEX + NORMAL * vec3(0.001);
 				painterly_vertex = VERTEX;
 			"
 	return ""
@@ -32,36 +33,54 @@ func _get_code(input_vars: Array[String], output_vars: Array[String], _mode: Sha
 				vec3 frag_normal = (INV_VIEW_MATRIX * vec4(NORMAL, 0.0)).xyz;
 				vec3 painterly_triplanar_power_normal = pow(abs(frag_normal), vec3(0.5));
 				painterly_triplanar_power_normal /= dot(painterly_triplanar_power_normal, vec3(1.0));
-				vec3 painterly_triplanar_pos = painterly_vertex * vec3({scale}) + {offset};
-				painterly_triplanar_pos *= vec3(1.0, -1.0, 1.0);
 
-				vec3 dir = clamp({direction}, -1.0, 1.0) * -1.0;
 				vec3 angle = (vec4(NORMAL, 1.0) * VIEW_MATRIX).xyz;
-				float dir_blend_factor = dot(dir, angle);
-				float dir_blend = mix(1.0, 0.0, dir_blend_factor);
+				vec3 offset = vec3(0.0);
 
-				vec4 paint_texture = painterly_triplanar_texture({paint_map}, painterly_triplanar_power_normal, painterly_triplanar_pos);
+				vec3 uv_r = painterly_vertex * vec3({scale_r}) + offset * vec3(1.0, -1.0, 1.0);
+				vec4 paint_r = painterly_triplanar_texture({paint_map}, painterly_triplanar_power_normal, uv_r);
+				float blend_r = smoothstep(paint_r.r, 1.0, 1.0 - (paint_r.r / {coverage_r})) * COLOR.r;
+				vec3 dir_r = clamp({direction_r}, vec3(-1.0), vec3(1.0)) * -1.0;
+				float dir_blend_r = mix(1.0, 0.0, dot((dir_r), angle));
 
-				int coverage_channel = int(clamp({coverage_channel}, 0, 3));
-				int brush_channel = int(clamp({brush_channel}, 0, 3));
-				float blend = {color}.a * smoothstep(paint_texture[coverage_channel], 1.0, 1.0 - (paint_texture[brush_channel] / {coverage}));
+				vec3 uv_g = painterly_vertex * vec3({scale_g}) + offset * vec3(1.0, -1.0, 1.0);
+				vec4 paint_g = painterly_triplanar_texture({paint_map}, painterly_triplanar_power_normal, uv_g) * COLOR.g;
+				float blend_g = smoothstep(paint_g.g, 1.0, 1.0 - (paint_g.g / {coverage_g}));
+				vec3 dir_g = clamp({direction_g}, -1.0, 1.0) * -1.0;
+				float dir_blend_g = mix(1.0, 0.0, dot((dir_g), angle));
 
-				vec4 mixed = mix({base_color}, {color}, vec4(blend) * dir_blend);
+				vec3 uv_b = painterly_vertex * vec3({scale_b}) + offset * vec3(1.0, -1.0, 1.0);
+				vec4 paint_b = painterly_triplanar_texture({paint_map}, painterly_triplanar_power_normal, uv_b) * COLOR.b;
+				float blend_b = smoothstep(paint_b.b, 1.0, 1.0 - (paint_b.b / {coverage_b}));
+				vec3 dir_b = clamp({direction_b}, -1.0, 1.0) * -1.0;
+				float dir_blend_b = mix(1.0, 0.0, dot((dir_b), angle));
+
+				vec3 dir_blend = vec3(dir_blend_r, dir_blend_g, dir_blend_b);
+
+				vec4 mixed = mix({base_color}, {color_r}, vec4({color_r}.a * blend_r) * dir_blend.r);
+				mixed = mix(mixed, {color_g}, vec4({color_g}.a * blend_g) * dir_blend.g);
+				mixed = mix(mixed, {color_b}, vec4({color_b}.a * blend_b) * dir_blend.b);
 
 				{out_color} = clamp(mixed, 0.0, 1.0);
-				{out_roughness} = mix({base_roughness}, {roughness_affect}, clamp(blend * dir_blend, 0.0, 1.0));
+				{out_color}.a = blend_r;
+				{out_roughness} = mix({base_roughness}, {paint_roughness}, clamp(blend_r * dir_blend.x, 0.0, 1.0));
 			".format({
 				"base_color": input_vars[0],
 				"base_roughness": input_vars[1],
-				"roughness_affect": input_vars[2],
-				"color": input_vars[3],
-				"coverage": input_vars[4],
-				"paint_map": input_vars[5],
-				"coverage_channel": input_vars[6],
-				"brush_channel": input_vars[7],
-				"direction": input_vars[8],
-				"scale": input_vars[9],
-				"offset": input_vars[10],
+				"paint_roughness": input_vars[2],
+				"color_r": input_vars[3],
+				"coverage_r": input_vars[4],
+				"scale_r": input_vars[5],
+				"direction_r": input_vars[6],
+				"color_g": input_vars[7],
+				"coverage_g": input_vars[8],
+				"scale_g": input_vars[9],
+				"direction_g": input_vars[10],
+				"color_b": input_vars[11],
+				"coverage_b": input_vars[12],
+				"scale_b": input_vars[13],
+				"direction_b": input_vars[14],
+				"paint_map": input_vars[15],
 				"out_color": output_vars[0],
 				"out_roughness": output_vars[1],
 			})
@@ -70,36 +89,46 @@ func _get_code(input_vars: Array[String], output_vars: Array[String], _mode: Sha
 func _get_description() -> String:
 	return "3 Layers of brushstrokes laid out using two different splats."
 
-func _get_input_port_count() -> int: return 11
+func _get_input_port_count() -> int: return 16
 
 func _get_input_port_default_value(port: int) -> Variant:
 	match port:
-		0: return Vector4.ZERO
+		0: return Vector4.ONE
 		1: return 0.5
 		2: return 0.5
-		3: return Vector4.ZERO
-		4: return 1.0
-		5: return null
-		6: return 1
-		7: return 1
-		8: return Vector3.ZERO
+		3: return Vector4.ONE
+		4: return 0.0
+		5: return 1.0
+		6: return Vector3.ZERO
+		7: return Vector4.ONE
+		8: return 0.0
 		9: return 1.0
 		10: return Vector3.ZERO
+		11: return Vector4.ONE
+		12: return 0.0
+		13: return 1.0
+		14: return Vector3.ZERO
+		15: return null
 	return null
 
 func _get_input_port_name(port: int) -> String:
 	match port:
 		0: return "Base Color"
 		1: return "Base Roughness"
-		2: return "Roughness Affect"
-		3: return "Color"
-		4: return "Coverage"
-		5: return "Paint Map"
-		6: return "Coverage Channel"
-		7: return "Brush Channel"
-		8: return "Direction"
-		9: return "Scale"
-		10: return "Offset"
+		2: return "Paint Roughness"
+		3: return "Color R"
+		4: return "Coverage R"
+		5: return "Scale R"
+		6: return "Direction R"
+		7: return "Color G"
+		8: return "Coverage G"
+		9: return "Scale G"
+		10: return "Direction G"
+		11: return "Color B"
+		12: return "Coverage B"
+		13: return "Scale B"
+		14: return "Direction B"
+		15: return "Paint Map"
 	return ""
 
 func _get_input_port_type(port: int) -> PortType:
@@ -109,12 +138,17 @@ func _get_input_port_type(port: int) -> PortType:
 		2: return PORT_TYPE_SCALAR
 		3: return PORT_TYPE_VECTOR_4D
 		4: return PORT_TYPE_SCALAR
-		5: return PORT_TYPE_SAMPLER
-		6: return PORT_TYPE_SCALAR
-		7: return PORT_TYPE_SCALAR
-		8: return PORT_TYPE_VECTOR_3D
+		5: return PORT_TYPE_SCALAR
+		6: return PORT_TYPE_VECTOR_3D
+		7: return PORT_TYPE_VECTOR_4D
+		8: return PORT_TYPE_SCALAR
 		9: return PORT_TYPE_SCALAR
 		10: return PORT_TYPE_VECTOR_3D
+		11: return PORT_TYPE_VECTOR_4D
+		12: return PORT_TYPE_SCALAR
+		13: return PORT_TYPE_SCALAR
+		14: return PORT_TYPE_VECTOR_3D
+		15: return PORT_TYPE_SAMPLER
 	return PORT_TYPE_SCALAR
 
 func _get_name() -> String: return "Painterly"
